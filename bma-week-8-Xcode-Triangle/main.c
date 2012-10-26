4b48548e4d37110e5337e9f111327c68e0578d16
Wallace Mooncai <wmooncai@alumni.rutgers.edu>
2012-10-26 19:28:47 -0400

    Initial Commit

---
+++ bma-week-8-Xcode-Triangle/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  bma-week-8-Xcode-Triangle
+//
+//  Created by Wally on 10/26/12.
+//  Copyright (c) 2012 W M. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
